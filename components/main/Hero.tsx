f7286c7230f80360234ef9ebbcb5607cd7eebfcc
Azure <paramasuraqutay@gmail.com>
2026-03-16 11:00:30 +0800

    update form error

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -218,7 +218,7 @@ function OverlayContent({ scrollYProgress }: { scrollYProgress: any }) {
                     Bali Blokchain Weeks
                 </h1>
                 <p className="text-xl tracking-[0.5em] mt-4 uppercase">
-                    Mid 2025
+                    Mid 2026
                 </p>
             </motion.div>
 
